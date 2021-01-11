ddf6323045f862a80842e667adf1a3f7bcd02726
godfathr <mikal.k.grant@outlook.com>
2021-01-10 20:01:58 -0700

    Started moving elements and styles to component.

--- stateSelect.js
+++ stateSelect.js
@@ -1,61 +1,107 @@
 const template = document.createElement('template');
 template.innerHTML = `
-        <div>
-            <label for="stateSelect">Select a State</label>
-            <select name="stateSelect" id="stateSelectList" class="stateSelectList">
-                <option value=""></option>
-                <option value="AL">Alabama</option>
-                <option value="AK">Alaska</option>
-                <option value="AZ">Arizona</option>
-                <option value="AR">Arkansas</option>
-                <option value="CA">California</option>
-                <option value="CO">Colorado</option>
-                <option value="CT">Connecticut</option>
-                <option value="DE">Delaware</option>
-                <option value="FL">Florida</option>
-                <option value="GA">Georgia</option>
-                <option value="HI">Hawaii</option>
-                <option value="ID">Idaho</option>
-                <option value="IL">Illinois</option>
-                <option value="IN">Indiana</option>
-                <option value="IA">Iowa</option>
-                <option value="KS">Kansas</option>
-                <option value="KY">Kentucky</option>
-                <option value="LA">Louisiana</option>
-                <option value="ME">Maine</option>
-                <option value="MD">Maryland</option>
-                <option value="MA">Massachusetts</option>
-                <option value="MI">Michigan</option>
-                <option value="MN">Minnesota</option>
-                <option value="MS">Mississippi</option>
-                <option value="MO">Missouri</option>
-                <option value="MT">Montana</option>
-                <option value="NE">Nebraska</option>
-                <option value="NV">Nevada</option>
-                <option value="NH">New Hampshire</option>
-                <option value="NJ">New Jersey</option>
-                <option value="NM">New Mexico</option>
-                <option value="NY">New York</option>
-                <option value="NC">North Carolina</option>
-                <option value="ND">North Dakota</option>
-                <option value="OH">Ohio</option>
-                <option value="OK">Oklahoma</option>
-                <option value="OR">Oregon</option>
-                <option value="PA">Pennsylvania</option>
-                <option value="RI">Rhode Island</option>
-                <option value="SC">South Carolina</option>
-                <option value="SD">South Dakota</option>
-                <option value="TN">Tennessee</option>
-                <option value="TX">Texas</option>
-                <option value="UT">Utah</option>
-                <option value="VT">Vermont</option>
-                <option value="VA">Virginia</option>
-                <option value="WA">Washington</option>
-                <option value="WV">West Virginia</option>
-                <option value="WI">Wisconsin</option>
-                <option value="WY">Wyoming</option>
-            </select>
-            <p><slot name="choice" /></p>
+<style>
+.optionContainer {
+    width: 300px;
+    max-width: 300px;
+    min-width: 150px;
+}
+
+#stateList {
+    display: none;
+}
+
+.stateListOptions {
+    border: 1px solid black;
+    margin-top: 0;
+    padding: 0;
+    background-color: lightgrey;
+    max-height: 300px;
+    width: calc(100% + 1rem);
+    overflow-y: scroll;
+    overflow-x: hidden;
+}
+
+input[type="text"] {
+    padding: 0.5rem;
+    width: 100%;
+}
+
+.optionList {
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+}
+
+.optionList li {
+    line-height: 1.8rem;
+    width: 100%;
+    padding: 0 1.3rem;
+}
+
+.optionList li:hover {
+    cursor: pointer;
+    background-color: black;
+    color: white;
+}
+        </style>
+        <div class="optionContainer">
+            <label for="stateSearch">Select a State</label>
+            <input type="text" id="stateSearch" value="" />
+            <div id="stateList" class="stateListOptions">
+                <ul class="optionList">
+                    <li id="AL" class="optionItem">Alabama</li>
+                    <li id="AK" class="optionItem">Alaska</li>
+                    <li id="AZ" class="optionItem">Arizona</li>
+                    <li id="AR" class="optionItem">Arkansas</li>
+                    <li id="CA" class="optionItem">California</li>
+                    <li id="CO" class="optionItem">Colorado</li>
+                    <li id="CT" class="optionItem">Connecticut</li>
+                    <li id="DE" class="optionItem">Delaware</li>
+                    <li id="FL" class="optionItem">Florida</li>
+                    <li id="GA" class="optionItem">Georgia</li>
+                    <li id="HI" class="optionItem">Hawaii</li>
+                    <li id="ID" class="optionItem">Idaho</li>
+                    <li id="IL" class="optionItem">Illinois</li>
+                    <li id="IN" class="optionItem">Indiana</li>
+                    <li id="IA" class="optionItem">Iowa</li>
+                    <li id="KS" class="optionItem">Kansas</li>
+                    <li id="KY" class="optionItem">Kentucky</li>
+                    <li id="LA" class="optionItem">Louisiana</li>
+                    <li id="ME" class="optionItem">Maine</li>
+                    <li id="MD" class="optionItem">Maryland</li>
+                    <li id="MA" class="optionItem">Massachusetts</li>
+                    <li id="MI" class="optionItem">Michigan</li>
+                    <li id="MN" class="optionItem">Minnesota</li>
+                    <li id="MS" class="optionItem">Mississippi</li>
+                    <li id="MO" class="optionItem">Missouri</li>
+                    <li id="MT" class="optionItem">Montana</li>
+                    <li id="NE" class="optionItem">Nebraska</li>
+                    <li id="NV" class="optionItem">Nevada</li>
+                    <li id="NH" class="optionItem">New Hampshire</li>
+                    <li id="NJ" class="optionItem">New Jersey</li>
+                    <li id="NM" class="optionItem">New Mexico</li>
+                    <li id="NY" class="optionItem">New York</li>
+                    <li id="NC" class="optionItem">North Carolina</li>
+                    <li id="ND" class="optionItem">North Dakota</li>
+                    <li id="OH" class="optionItem">Ohio</li>
+                    <li id="OK" class="optionItem">Oklahoma</li>
+                    <li id="OR" class="optionItem">Oregon</li>
+                    <li id="PA" class="optionItem">Pennsylvania</li>
+                    <li id="RI" class="optionItem">Rhode Island</li>
+                    <li id="SC" class="optionItem">South Carolina</li>
+                    <li id="SD" class="optionItem">South Dakota</li>
+                    <li id="TN" class="optionItem">Tennessee</li>
+                    <li id="TX" class="optionItem">Texas</li>
+                    <li id="UT" class="optionItem">Utah</li>
+                    <li id="VT" class="optionItem">Vermont</li>
+                    <li id="VA" class="optionItem">Virginia</li>
+                    <li id="WA" class="optionItem">Washington</li>
+                    <li id="WV" class="optionItem">West Virginia</li>
+                    <li id="WI" class="optionItem">Wisconsin</li>
+                    <li id="WY" class="optionItem">Wyoming</li>
+                </ul>
+            </div>
         </div>
 `;
 
@@ -64,8 +110,12 @@ class StateList extends HTMLElement {
         // Always call super first in constructor
         super();
 
+        // this.innerHTML = ``;
+
         this.attachShadow({ mode: 'open' });
         this.shadowRoot.appendChild(template.content.cloneNode(true));
+        let searchBox = this.shadowRoot.querySelector("#stateSearch");
+        console.log(searchBox);
         /*let selectOptions = this.shadowRoot.getElementById('stateSelectList');
         for (var i = 0; i < selectOptions; i++) {
             console.log(option.value);
@@ -86,11 +136,11 @@ class StateList extends HTMLElement {
     }
 
     connectedCallback() {
-        this.shadowRoot.querySelector('#stateSelectList').addEventListener('click', () => this.updateSelection());
+        //this.shadowRoot.querySelector('#stateSelectList').addEventListener('click', () => this.updateSelection());
     }
 
     disconnectedCallback() {
-        this.shadowRoot.querySelector('#stateSelectList').removeEventListener();
+        //this.shadowRoot.querySelector('#stateSelectList').removeEventListener();
     }
 }
 
